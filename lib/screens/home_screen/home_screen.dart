3183ea6bb6e390e070fe31ca433eabfadc1bf884
Sreehari Rajev <62306976+SreehariRajeev@users.noreply.github.com>
2025-09-22 11:52:18 +0530

    Update home_screen.dart

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
     return GetBuilder<HomeScreenController>(
       builder: (controller) {
         return Scaffold(
-          appBar: AppBar(title: Text("Home")),
+          appBar: AppBar(title: Text("HOME")),
           body:
               controller.isLoading
                   ? CircularProgressIndicator.adaptive()
